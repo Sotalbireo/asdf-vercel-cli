56613b1b972ba6e03c9932c82ee10260d692fcd7
sasaky <athene609.cyl@gmail.com>
2021-10-03 22:18:36 +0900

    :sparkles: solves todo

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for vercel-cli.
-GH_REPO="https://github.com/vercel/vercel/tree/main/packages/cli"
+# Ensure this is the correct GitHub homepage where releases can be downloaded for vercel-cli.
+GH_REPO="https://github.com/vercel/vercel"
 TOOL_NAME="vercel-cli"
 TOOL_TEST="vercel --help"
 
@@ -31,7 +31,7 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
+  # Adapt this. By default we simply list the tag names from GitHub releases.
   # Change this function if vercel-cli has other means of determining installable versions.
   list_github_tags
 }
@@ -41,7 +41,7 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for vercel-cli
+  # Adapt the release URL convention for vercel-cli
   url="$GH_REPO/archive/v${version}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
@@ -61,7 +61,7 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert vercel-cli executable exists.
+    # Asert vercel-cli executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
